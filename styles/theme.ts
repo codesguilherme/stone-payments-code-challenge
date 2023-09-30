93523fe918f29382f7f360913f2bc490dd1d2032
Guilherme Oliveira <coder.guilherme@gmail.com>
2023-09-29 21:22:48 -0300

    chore(styles): update theme colors and spacing

--- styles/theme.ts
+++ styles/theme.ts
@@ -8,12 +8,32 @@ const Theme = {
     mediumGray2: '#8C9CAD',
     mediumGray3: '#D7E0EB',
     darkGray: '#2E3742',
+    darkGray2: '#45505E',
     background: '#ffffff'
   },
   shadows: {
     medium: '0 8px 14px rgba(0, 0, 0, .16)',
     small: '0 4px 8px rgba(0, 0, 0, .16)',
     tiny: '0 1px 1px rgba(0, 0, 0, .16)'
+  },
+  spacing: {
+    0: '0px',
+    1: '4px',
+    2: '8px',
+    3: '16px',
+    4: '24px',
+    5: '32px',
+    6: '48px',
+    7: '64px',
+    8: '128px',
+    9: '256px',
+    10: '512px',
+    default: '8px',
+    tiny: '2px',
+    small: '4px',
+    medium: '16px',
+    large: '24px',
+    huge: '56px'
   }
 };
 
