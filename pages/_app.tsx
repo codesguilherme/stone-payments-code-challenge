7f672a1d53bd39524ec0f61c7a46a9bce397f477
Guilherme Oliveira <coder.guilherme@gmail.com>
2023-10-05 21:45:16 -0300

    chore: add favicon and page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <Fragment>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Stone Code Challenge</title>
       </Head>
       <ThemeProvider theme={Theme}>
         <GlobalStyles />
